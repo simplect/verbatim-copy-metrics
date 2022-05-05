d7cb7b86454f7ef5219cf91bb26ae9d999cae05e
Merijn Testroote <hi@merijn.site>
2022-05-05 10:56:49 +0200

    First experimental set-up and global statistics.

---
+++ code/data.py
@@ -0,0 +1,123 @@
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Plot parameters
+plt.rcParams["figure.dpi"] = 320 # Dependent on screen
+plt.rcParams["figure.constrained_layout.use"] = True
+
+# Configuration
+config_simulations_path = '/Users/merijn/stack/3. Studie/36._thesis/36.04 simulations/QS_Simulation4Verbatim'
+config_training_images = ['stone', 'strebelle']
+config_training_parameters = [0]
+
+# Parse filenames
+files = [file for file in os.listdir(config_simulations_path) if file[0:5] == 'qsSim']
+file_parameters = np.array([file[6:][:-4].split('_') for file in files if file[0:5] == 'qsSim'])
+file_parameters
+df = pd.DataFrame({'training_image_type': file_parameters[:, 0],
+                   'simulation_parameters': file_parameters[:, 1],
+                   'index_map': np.NAN})
+# Keep only stone or strebelle, as their index map sizes are different
+df = df[df['training_image_type'] == 'stone']
+# Create a random sample
+df = df.sample(n=100, replace=True)
+
+# %%
+def get_simulation_maps(training_image_type, parameter):
+    sim_result = np.load(f'{config_simulations_path}/qsSim_{training_image_type}_{parameter}.npz')
+    # Per simulation there are many simulations, for now we sample one
+    i = np.random.randint(sim_result['indexMap'].shape[0])
+    _index_map = sim_result['indexMap'][i, :, :]
+    _training_image = sim_result['ti']
+    _sim_image = sim_result['sim'][i, :, :]
+    return _training_image, _sim_image, _index_map
+
+
+def get_index_map(training_image_type, parameter):
+    _, _, _index_map = get_simulation_maps(training_image_type, parameter)
+    return _index_map
+
+# Collect the index maps for all the samples
+df.loc[:, 'index_map'] = df.apply(lambda x: get_index_map(x['training_image_type'],
+                                                       x['simulation_parameters']), axis=1)
+
+#%%
+def plot_sim_index(training_image, sim_image, index_map):
+    sourceIndex = np.stack(
+        np.meshgrid(np.arange(training_image.shape[0]) / training_image.shape[0],
+                    np.arange(training_image.shape[1]) / training_image.shape[1]) +
+        [np.ones_like(training_image)],
+        axis=-1);
+    fig, axs = plt.subplots(2, 2)
+    ax1 = plt.subplot(221)
+    ax1.imshow(training_image)
+    ax1.set_title('Training image')
+    ax1.axis('off')
+    ax2 = plt.subplot(222)
+    ax2.imshow(sim_image)
+    ax2.set_title('Simulation')
+    ax2.axis('off')
+
+    ax3 = plt.subplot(223)
+    ax3.imshow(sourceIndex)
+    ax3.set_title('Index origin')
+    ax3.axis('off')
+    ax3 = plt.subplot(224)
+    ax3.imshow(index_map)
+    ax3.imshow(sourceIndex.reshape(-1,3)[index_map])
+    ax3.set_title('Index map')
+    ax3.axis('off')
+    plt.show()
+
+
+training_image, sim_image, index_map = get_simulation_maps('stone', df.sample().iloc[0].at['simulation_parameters'])
+plot_sim_index(training_image, sim_image, index_map)
+#%%
+plt.hist(np.sort(index_map.reshape(-1)), bins=100)
+plt.show()
+
+
+#%%
+from sklearn import linear_model
+
+def histogram_metric(index_map_1d):
+    X = np.log(np.sort(np.histogram(index_map_1d, bins=100)[0]))
+    lm = linear_model.LinearRegression()
+    X = X[X > 0]
+    lm.fit(X.reshape(-1, 1), range(len(X)))  # fitting the model
+    return lm.coef_
+
+histogram_metric(index_map)
+
+#%%
+plt.plot(np.sort(np.histogram(index_map.reshape(-1), bins=100)[0]))
+plt.show()
+
+#%%
+df['hist_coef'] = np.reshape(df['index_map'], -1).apply(histogram_metric)
+
+lowest = df.sort_values('hist_coef').iloc[0]
+
+highest  = df.sort_values('hist_coef').iloc[-1]
+ax = plt.subplot(221)
+ax.imshow(lowest['index_map'])
+ax.axis('off')
+ax = plt.subplot(222)
+ax.imshow(highest['index_map'])
+ax.axis('off')
+
+ax = plt.subplot(223)
+index_map_1d = lowest['index_map'].reshape(-1)
+X = np.log(np.sort(np.histogram(index_map_1d, bins=100)[0]))
+ax.plot(X)
+ax.axis('off')
+
+ax = plt.subplot(224)
+index_map_1d = highest['index_map'].reshape(-1)
+X = np.log(np.sort(np.histogram(index_map_1d, bins=100)[0]))
+ax.plot(X)
+ax.axis('off')
+plt.show()
+
